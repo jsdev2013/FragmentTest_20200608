aec2fa065f00866afa220f2691d18b9583c14f60
JISU LEE <jsnote2013@gmail.com>
2020-06-08 21:51:31 +0900

    페이지별 제목을 어뎁터에서 설정

--- app/src/main/java/com/jisu/fragmenttest_20200608/adapter/MyPagerAdapter.kt
+++ app/src/main/java/com/jisu/fragmenttest_20200608/adapter/MyPagerAdapter.kt
@@ -7,6 +7,14 @@ import com.jisu.fragmenttest_20200608.fragments.FirstFragment
 import com.jisu.fragmenttest_20200608.fragments.SecondFragment
 
 class MyPagerAdapter (fm: FragmentManager) :FragmentPagerAdapter(fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0 -> "프로필 화면"
+            else -> "수강생 목록"
+        }
+    }
+
     override fun getItem(position: Int): Fragment {
         return when(position){
             0 -> FirstFragment()
